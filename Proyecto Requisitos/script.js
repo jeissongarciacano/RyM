c05683df8746cb19fc5f8b5f2a186995f2b67a43
jeissongarciacano <47406585+jeissongarciacano@users.noreply.github.com>
2019-10-27 15:53:49 -0500

    ...

--- Proyecto Requisitos/script.js
+++ Proyecto Requisitos/script.js
@@ -4,4 +4,22 @@ var connect = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-})
+    database: 'database',
+});
+RTCPeerConnection.connect(function(error){
+    if(!!error){
+        console.log('error');
+    } else{
+        console.log('connected');
+    }
+});
+app.get('/', function(req, resp){
+    connection.query("SELECT * FROM database", function(error,rows, fields){
+        if(!!error){
+            console.log('Error in the query');
+        }else{
+            console.log('successful query');
+        }
+    });
+});
+app.listen(1337);
